300dc2b81e94372d72433240659a0e85eee311d8
Phan Văn Toàn <toanyogame@gmail.com>
2024-07-25 14:47:03 +0700

    fix width tour item

--- components/TourItem.tsx
+++ components/TourItem.tsx
@@ -73,7 +73,7 @@ const TourItem = ({tour}: {tour: Tour}) => {
     return (
         <View className='mt-3 mb-3 p-1 w-full'>
             <View className="flex-col py-1 px-1 rounded-lg border-gray-700 border-2 w-full">
-                <View className="flex-row items-center justify-between py-4 px-2">
+                <View className="flex-row items-center justify-between w-[90%] py-4 px-2 ">
                     <View className="bg-[#F2F2F2] rounded-lg py-1 px-3">
                         <Text className="text-base font-bold text-center">{getMonthName(startDate).slice(0, 3)}</Text>
                         <Text className="text-3xl font-bold text-center text-[#FFC626]">{startDate.getUTCDate()}</Text>
